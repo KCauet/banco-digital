e2b5bea70da8d612668ba32914bb6c4c42b10f00
KCauet <keirrisoncauet31@gmail.com>
2026-07-17 19:16:53 -0300

    Initial cleaning to develop

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className='centerBox'>
+          <section>
+            <h1>Bank LowCost</h1>
+          </section>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
