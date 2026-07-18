2c91d419e9a9b9fb496724a0f6a5e3559d83594d
KCauet <keirrisoncauet31@gmail.com>
2026-07-18 06:16:53 -0300

    Basic layout done, time for the logic part

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import HistoryCard from './components/HistoryCard' // PLESA DON'T FORGET TO UPPERCASE THE FIRST LETTER OF THE COMPONENT :<
 
 function App() {
 
@@ -7,9 +8,52 @@ function App() {
     <>
       <main>
         <div className='centerBox'>
-          <section>
+
+          <section className='titleBox'>
             <h1>Bank LowCost</h1>
           </section>
+
+          <section className='userInfoBox'>
+            <h1>Your Total Balance: 2500R$</h1>
+          </section>
+          
+          <section className='inputBox'>
+            
+            <div>
+              <h1>Value:</h1>
+              <input type="number" />
+            </div>
+            <div>
+              <button>Deposit</button>
+              <button>Withdraw</button>
+            </div>
+            
+          </section>
+          
+          <section>
+            <h1>History</h1>
+          </section>
+
+          <section className='historyCards'>
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+            <HistoryCard />
+          </section>
+        
         </div>
       </main>
     </>
---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function HistoryCard() {
+    return (
+        <>
+        <div className='historyCardBox'>
+            <h2>Value here</h2>
+        </div>
+        </>
+    );
+}
+
+export default HistoryCard
