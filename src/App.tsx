5c341dfbe58f9265b59259ff43089e32734943df
KCauet <keirrisoncauet31@gmail.com>
2026-07-18 06:45:18 -0300

    Working deposit & withdraw system, close to finish

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,33 @@ import HistoryCard from './components/HistoryCard' // PLESA DON'T FORGET TO UPPE
 
 function App() {
 
+  const [userBalance, setUserBalance] = useState<number>(0);
+  const [inputValue, setInputValue] = useState<number>(0);
+
+  function depositValue(value: number) {
+    if (value > 0) {
+      setUserBalance(userBalance + value);
+      setInputValue(0);
+    } else {
+      alert("You can't deposit 0 dollars")
+    }
+  }
+
+  function withdrawValue(value: number) {
+    if (value <= userBalance && value != 0) {
+      setUserBalance(userBalance - value);
+      setInputValue(0);
+      alert('Successfull transation')
+    } else {
+      if (value > userBalance) {
+        alert("Insufficient money")
+      } else {
+        alert("You can't withdraw 0 dollars")
+      }
+
+    }
+  }
+
   return (
     <>
       <main>
@@ -14,44 +41,28 @@ function App() {
           </section>
 
           <section className='userInfoBox'>
-            <h1>Your Total Balance: 2500R$</h1>
+            <h1>Your Total Balance: {userBalance}$</h1>
           </section>
           
           <section className='inputBox'>
             
             <div>
               <h1>Value:</h1>
-              <input type="number" />
+              <input type="Number" 
+              value={inputValue}
+              onChange={(event) => {setInputValue(Number(event.target.value))}}
+              />
             </div>
             <div>
-              <button>Deposit</button>
-              <button>Withdraw</button>
+              <button onClick={() => {depositValue(inputValue)}} >Deposit</button>
+              <button onClick={() => {withdrawValue(inputValue)}} >Withdraw</button>
             </div>
-            
-          </section>
-          
-          <section>
+
             <h1>History</h1>
           </section>
 
           <section className='historyCards'>
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
-            <HistoryCard />
+            
           </section>
         
         </div>
